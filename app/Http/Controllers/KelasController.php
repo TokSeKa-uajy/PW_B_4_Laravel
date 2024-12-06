66c677ebaf07b4095aef16197d264725a4cbedd4
TokSeKa-uajy <220711904@students.uajy.ac.id>
2024-12-06 20:57:18 +0700

    Update KelasController.php

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -2,9 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kelas; // Pastikan model Kelas tersedia
 use Illuminate\Http\Request;
 
 class KelasController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kelas = Kelas::all();
+        return response()->json($kelas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // Untuk API biasanya tidak perlu method create
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $kelas = Kelas::create($request->all());
+        return response()->json($kelas, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $kelas = Kelas::find($id);
+        if (!$kelas) {
+            return response()->json('Kelas not found', 404);
+        }
+        return response()->json($kelas);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Untuk API biasanya tidak perlu method edit
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $kelas = Kelas::find($id);
+        if (!$kelas) {
+            return response()->json('Kelas not found', 404);
+        }
+        $kelas->update($request->all());
+        return response()->json($kelas);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $kelas = Kelas::find($id);
+        if (!$kelas) {
+            return response()->json('Kelas not found', 404);
+        }
+        $kelas->delete();
+        return response()->json('Kelas deleted successfully');
+    }
 }
+
